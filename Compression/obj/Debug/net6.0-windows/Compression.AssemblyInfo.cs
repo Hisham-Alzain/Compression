34ece6e415e8ee602b08759b369b2472142603f9
Ghassan-123 <ghassankk12342003@gmail.com>
2025-06-18 11:19:46 +0300

    updated the version

--- Compression/obj/Debug/net6.0-windows/Compression.AssemblyInfo.cs
+++ Compression/obj/Debug/net6.0-windows/Compression.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Compression")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dbb34bdcc92e15ce70640db1e3a67b73208348a6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Compression")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Compression")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
